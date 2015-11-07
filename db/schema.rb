66cb3f38cc833845d8e30b5d0be5fd9533143825
Jose Paradas <jparadas.b@gmail.com>
2015-11-07 15:03:46 -0300

    Configurado Procfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150801214723) do
 
   create_table "bancos", force: :cascade do |t|
@@ -101,7 +100,6 @@ ActiveRecord::Schema.define(version: 20150801214723) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 
   add_foreign_key "documents", "folders"
   add_foreign_key "orders", "payments"
